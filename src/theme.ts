3294d4b47a508c71b4725f01a8eaedadf7afc731
avocarlos <cgarciasantander@gmail.com>
2023-03-23 18:15:44 -0500

    Dropping cra over vite

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { grey } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
+
+// A custom theme for this app
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: grey[900]
+    }
+  }
+});
+
+export default theme;

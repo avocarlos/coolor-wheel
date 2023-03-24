e35dfa9cad9ac6515d146730878ca810f3c1b3f8
avocarlos <cgarciasantander@gmail.com>
2023-03-24 17:42:04 -0500

    Fixing merge conflicts

--- src/context/ColorContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const ColorContext = React.createContext({})
-
-export const ColorProvider = ColorContext.Provider;
-export const ColorConsumer = ColorContext.Consumer;
-export default ColorContext;
